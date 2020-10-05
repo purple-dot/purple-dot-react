9d6cdf012a69c84f3d047a59b1f27e5b05c36470
Dominik Krejcik <dominik.krejcik@gmail.com>
2020-10-05 16:35:04 +0100

    feat: add messaging placement component

--- e2e/index.jsx
+++ e2e/index.jsx
@@ -1,10 +1,12 @@
-import { PurpleDot, PriceElement } from '@purple-dot/purple-dot-react';
+import { PurpleDot, PriceElement, ButtonElement, MessagingElement } from '@purple-dot/purple-dot-react';
 import React from '../node_modules/react';
 import ReactDOM from '../node_modules/react-dom';
 
 const App = () => (
   <PurpleDot apiKey="test-api-key">
     <PriceElement sku="test-sku" />
+    <ButtonElement sku="test-sku" />
+    <MessagingElement sku="test-sku" />
   </PurpleDot>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import PurpleDot, { usePurpleDot } from './components/PurpleDotContext';
 import PriceElement from './components/PriceElement';
 import ButtonElement from './components/ButtonElement';
+import MessagingElement from './components/MessagingElement';
 
 export {
   PurpleDot,
   usePurpleDot,
   PriceElement,
   ButtonElement,
+  MessagingElement,
 };
