6f27b46c8aac3ee21340551a51bf8dd08677e463
Dominik Krejcik <dominik.krejcik@gmail.com>
2021-11-09 15:57:34 +0000

    feat: add support for the cart

--- e2e/index.jsx
+++ e2e/index.jsx
@@ -1,24 +1,34 @@
 import {
-  PurpleDot, PriceElement, ButtonElement, PreorderStatus,
+  PurpleDot, PriceElement, ButtonElement, CartButtonElement, PreorderStatus,
 } from '@purple-dot/purple-dot-react';
-import React from '../node_modules/react';
+import React, { useState } from '../node_modules/react';
 import ReactDOM from '../node_modules/react-dom';
 
 // API Key is for Demo sandbox store
-const App = () => (
-  <PurpleDot apiKey="b351faa2-8693-4c09-b814-759beed90d0b">
-    { window.location.href.includes('/pre-order-status') ? (
-      <PreorderStatus email="test@email.com" />
-    ) : (
-      <>
-        <PriceElement sku="test-sku" />
-        <ButtonElement
-          sku="test-sku"
-          lineItemProperties={[{ name: 'Foo', value: 'Bar' }]}
-        />
-      </>
-    )}
-  </PurpleDot>
-);
+const App = () => {
+  const [cartButtonVisible, setCartButtonVisible] = useState(true);
+
+  return (
+    <PurpleDot apiKey="b351faa2-8693-4c09-b814-759beed90d0b" enableCart>
+      { window.location.href.includes('/pre-order-status') ? (
+        <PreorderStatus email="test@email.com" />
+      ) : (
+        <>
+          <div className="cart-button-container" style={{ display: cartButtonVisible ? 'block' : 'none' }}>
+            <CartButtonElement
+              onVisibilityChanged={({ showing }) => setCartButtonVisible(showing)}
+            />
+          </div>
+
+          <PriceElement sku="test-sku" />
+          <ButtonElement
+            sku="test-sku"
+            lineItemProperties={[{ name: 'Foo', value: 'Bar' }]}
+          />
+        </>
+      )}
+    </PurpleDot>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import PurpleDot, { usePurpleDot } from './components/PurpleDotContext';
 import PreorderStatus from './components/PreorderStatus';
 import PriceElement from './components/PriceElement';
 import ButtonElement from './components/ButtonElement';
+import CartButtonElement from './components/CartButton';
 
 export {
   PurpleDot,
   usePurpleDot,
   PriceElement,
   ButtonElement,
+  CartButtonElement,
   PreorderStatus,
 };
